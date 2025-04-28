6e088320322214c13e2bce54e7cf76ac969dcabc
Kan-O435 <ky.20626314@gmail.com>
2025-04-28 14:05:21 +0900

    create,usermodel

---
+++ db/migrate/20250427023831_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+
+      t.string:name,null:false
+      t.string:mail,null:false
+      t.string:password_digest,null:false  #digestで認証化できるらしい
+
+      t.timestamps
+    end
+  end
+end
