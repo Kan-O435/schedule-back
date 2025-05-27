67bd45298193799bfde9573c7b349d30d283ed0f
Kan-O435 <ky.20626314@gmail.com>
2025-05-28 04:40:26 +0900

    fix/cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # フロントのURL（開発環境に合わせる）
+    origins 'http://localhost:5173'  # Reactの起動URL（Viteのデフォルト）
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # cookieや認証情報を送るならtrue
+      credentials: true  # 認証情報（Cookieなど）を送る場合はtrue
   end
 end
