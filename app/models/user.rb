d3df84ab7a491b445d6ab25dcf95ec20dbdbc58d
Kan-O435 <ky.20626314@gmail.com>
2025-07-22 17:31:46 +0900

    fix/usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :share_plans
   has_many :shared_plans, through: :share_plans, source: :plan
 
+  
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
